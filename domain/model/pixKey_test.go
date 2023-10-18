cddaa83bfd4123a8e4eaec96593f19d64139a831
Lucas Pinho <lucas.pinho@starti.com.br>
2023-10-18 09:08:35 -0300

    :sparkles: feat: Adding `pixKey` model and test.

---
+++ domain/model/pixKey_test.go
@@ -0,0 +1,34 @@
+package model_test
+
+import (
+	uuid "github.com/satori/go.uuid"
+	"testing"
+
+	"github.com/lhps/codepix-go/domain/model"
+	"github.com/stretchr/testify/require"
+)
+
+func TestModel_NewPixKey(t *testing.T) {
+	code := "001"
+	name := "Banco do Brasil"
+	bank, err := model.NewBank(code, name)
+
+	accountNumber := "abcnumber"
+	ownerName := "Lucas"
+	account, err := model.NewAccount(bank, accountNumber, ownerName)
+
+	kind := "email"
+	key := "l@pinho.com"
+	pixKey, err := model.NewPixKey(kind, key, account)
+
+	require.NotEmpty(t, uuid.FromStringOrNil(pixKey.ID))
+	require.Equal(t, pixKey.Kind, kind)
+	require.Equal(t, pixKey.Status, "active")
+
+	kind = "cpf"
+	_, err = model.NewPixKey(kind, key, account)
+	require.Nil(t, err)
+
+	_, err = model.NewPixKey("nome", key, account)
+	require.NotNil(t, err)
+}
